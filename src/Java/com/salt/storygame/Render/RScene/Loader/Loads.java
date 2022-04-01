aa712b3da6300f490184bb81705df91c04db47e6
anup <anupcpilla@gmail.com>
2022-04-01 08:40:57 -0400

    Initial Commit

--- src/Java/com/salt/storygame/Render/RScene/Loader/Loads.java
+++ src/Java/com/salt/storygame/Render/RScene/Loader/Loads.java
@@ -9,6 +9,8 @@ import com.salt.storygame.Render.org.Chunk;
 
 import java.awt.image.BufferedImage;
 
+import static
+
 public class Loads {
     static BufferedImageLoader bil = new BufferedImageLoader();
     public static void load(int i, IGameObjectHandler handler) {
@@ -51,5 +53,31 @@ public class Loads {
         }
     }
 
-
+    private static void MountainVillage(IGameObjectHandler handler) {
+        handler.destroy();
+        // Load level based on img
+        int w = bil.loadImage("/sampl.png").getWidth();
+        int h = bil.loadImage("/sampl.png").getHeight();
+        int c = 1;
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                int pixel = bil.loadImage("/sampl.png").getRGB(i, j);
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+                if (red == 255 && green == 0 && blue == 0) {
+                    handler.add(new Border(i*32, j*32, 32, 32, ID.Border, Textures.border, c));
+                } if (red == 255 && blue == 0 && green == 255) {
+                    handler.add(new Block(i*32, j*32, 32, 32, ID.Block, Textures.wall1, c));
+                } if (red == 0 && blue == 255 && green == 0) {
+                    handler.add(new Player(i*32, j*32, 32, 32, ID.Player, handler, null, c));
+                } if (red == 0 && green == 255 && blue == 0) {
+                    handler.add(new Button(i*32, j*32, 32, 32, ID.Button, null, Textures.button_wall1, Textures.button_wall2, c));
+                } if (red == 0 && green == 255 && blue == 255) {
+                    handler.add(new Door(i*32, j*32, 32, 32, ID.Door, Textures.wall1, Textures.NULL, c));
+                }
+                c++;
+            }
+        }
+    }
 }
